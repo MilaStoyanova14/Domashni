791dd34ca1f92c6769c3b751a164d417eab50a07
Pesho <p.petrov@alphavision.bg>
2019-11-12 20:38:14 +0200

    [Pesho] Testing

---
+++ Upr4/Upr4_1/Upr4_1/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Upr4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_pal = new System.Windows.Forms.Button();
+            this.button_ascii = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_pal
+            // 
+            this.button_pal.Location = new System.Drawing.Point(45, 12);
+            this.button_pal.Name = "button_pal";
+            this.button_pal.Size = new System.Drawing.Size(85, 27);
+            this.button_pal.TabIndex = 0;
+            this.button_pal.Text = "Палиндром";
+            this.button_pal.UseVisualStyleBackColor = true;
+            this.button_pal.Click += new System.EventHandler(this.button_pal_Click);
+            // 
+            // button_ascii
+            // 
+            this.button_ascii.Location = new System.Drawing.Point(45, 160);
+            this.button_ascii.Name = "button_ascii";
+            this.button_ascii.Size = new System.Drawing.Size(78, 30);
+            this.button_ascii.TabIndex = 1;
+            this.button_ascii.Text = "ASCII код";
+            this.button_ascii.UseVisualStyleBackColor = true;
+            this.button_ascii.Click += new System.EventHandler(this.button_ascii_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(45, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 212);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_ascii);
+            this.Controls.Add(this.button_pal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_pal;
+        private System.Windows.Forms.Button button_ascii;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Upr4/Upr4_1/Upr4_1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upr4_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+  
+
+        private void button_pal_Click(object sender, EventArgs e)
+        {
+            String s = textBox1.Text;
+            if (s != "")
+            {
+                
+                s = s.ToUpper();
+                char[] arr = s.ToCharArray();
+                Array.Reverse(arr);
+                string new_s = new string(arr);
+                int i = s.CompareTo(new_s);
+                if (i == 0)
+                {
+                    MessageBox.Show("Палиндром е!");
+                }
+                else MessageBox.Show("НЕ е Палиндром!");
+            }
+            else {
+                    MessageBox.Show("Моля въведете нещо в полето!");
+                }
+            
+        }
+
+        private void button_ascii_Click(object sender, EventArgs e)
+        {
+            String s = textBox1.Text;
+            if (s != "")
+            {
+
+                String msg = "";
+                foreach (char val in s)
+                {
+                    msg += val.ToString() + " = " + ((int)val).ToString() + " \n";
+
+                }
+
+                MessageBox.Show(msg);
+            }
+            else {
+                MessageBox.Show("Моля въведете нещо в полето!");
+            }
+
+        }
+    }
+}
