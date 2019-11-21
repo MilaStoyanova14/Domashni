870d895b2ac883463f18fbb0ccc8bb82dfba9ddf
Mila Stoyanova <mila.stoyanova14@abv.bg>
2019-11-21 16:23:41 +0200

    added Upr7 and Upr8

---
+++ Upr7/Upr7_2/Upr7_2/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upr7_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime myDT = new DateTime(dateTimePicker1.Value.Date.Year, dateTimePicker1.Value.Date.Month, dateTimePicker1.Value.Date.Day, new GregorianCalendar());
+            JulianCalendar myCal = new JulianCalendar();
+            textBox1.Text = "Day: " + myCal.GetDayOfYear(myDT);
+        }
+    }
+}
---
+++ Upr8/Upr8_1_2/Upr8_1_2/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Upr8_1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
